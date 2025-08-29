ee2945b5165b3b393df5d8befd2b380245bba192
Anton Burtsev <aburtsev@cs.utah.edu>
2025-08-29 15:33:29 -0600

    HW1 template

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,10 @@
 #![cfg_attr(not(test), no_std, no_main)]
 
-#[macro_use]
-mod console;
-
-use core::panic::PanicInfo;
-
 #[unsafe(no_mangle)]
 pub extern "C" fn rust_main() -> ! {
-    println!("Hello, world from Rust!");
+    println!("Hello from Rust!");
 
     loop {}
 }
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
+
