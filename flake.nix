0139669f45b65391c6da4fb59882e188d8fa4222
Zhaofeng Li <hello@zhaofeng.li>
2025-09-09 22:34:39 -0600

    flake.nix: Add autograder shell

--- flake.nix
+++ flake.nix
@@ -39,25 +39,71 @@
       extensions = [ "rust-src" "rust-analyzer-preview" ];
       targets = [ "x86_64-unknown-linux-gnu" ];
     };
+
+    rustNightlyMinimal = pkgs.rust-bin.nightly.${nightlyVersion}.minimal.override {
+      extensions = [ "rust-src" ];
+      targets = [ "x86_64-unknown-linux-gnu" ];
+    };
+
+    commonTools = with pkgs; [
+      # Toolchain
+      nasm
+      (pkgs.writeShellScriptBin "x86_64.ld" ''
+        exec ${x86CrossPkgs.buildPackages.bintools}/bin/${x86CrossPkgs.stdenv.cc.targetPrefix}ld "$@"
+      '')
+    ];
   in {
     devShell = x86CrossPkgs.mkShell {
       nativeBuildInputs = with pkgs; ([
         rustNightly
-
         qemu
         gdb
-
-        # Toolchain
-        nasm
-        (pkgs.writeShellScriptBin "x86_64.ld" ''
-          exec ${x86CrossPkgs.buildPackages.bintools}/bin/${x86CrossPkgs.stdenv.cc.targetPrefix}ld "$@"
-        '')
-      ] ++ lib.optionals pkgs.stdenv.isLinux [
+      ] ++ commonTools ++ lib.optionals pkgs.stdenv.isLinux [
         grub2
         xorriso
       ]);
 
       GRUB_X86_MODULES = lib.optionalString pkgs.stdenv.isLinux "${x86Pkgs.grub2}/lib/grub/i386-pc";
+    };
+    devShells.autograder = x86CrossPkgs.mkShell {
+      nativeBuildInputs = with pkgs; [
+        rustNightlyMinimal
+        qemu_test
+      ] ++ commonTools;
+
+      # Ugly hack to prebuild a shell
+      buildPhase = ''
+        mkdir -p $out
+        (
+          # nix/src/nix-build/nix-build.cc
+          unset TZ
+          unset NIX_ENFORCE_PURITY
+          export dontAddDisableDepTrack=1
+
+          # Variables whose values we cannot dictate here
+          unset SHELL
+          unset TERM
+          unset SHLVL
+          unset PWD
+          unset OLDPWD
+
+          unset HOME
+
+          unset TMP
+          unset TEMP
+          unset TMPDIR
+          unset TEMPDIR
+
+          unset NIX_BUILD_TOP
+          unset NIX_BUILD_CORES
+
+          unset NIX_LOG_FD
+
+          echo 'OLDPATH="$PATH"' >>$out/profile
+          declare -x >>$out/profile
+          echo 'export PATH="$PATH:$OLDPATH"' >>$out/profile
+        )
+      '';
     };
   });
 }
