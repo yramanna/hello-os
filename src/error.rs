a3b3d0b7c29ac92e29db60244b95e5195f568560
Anton Burtsev <aburtsev@cs.utah.edu>
2025-10-02 10:50:21 -0600

    Remove boot cmd error

--- src/error.rs
+++ src/error.rs
@@ -4,8 +4,6 @@ pub type Result<T> = core::result::Result<T, Error>;
 
 use displaydoc::Display;
 
-use crate::boot::command_line::Component as CommandLineComponent;
-
 /// An error.
 #[non_exhaustive]
 #[derive(Clone, Debug, Display)]
@@ -13,8 +11,6 @@ pub enum Error {
     /// No such script is defined.
     NoSuchScript,
 
-    /// Invalid kernel command-line component: {0:?}
-    InvalidCommandLineOption(CommandLineComponent<'static>),
 
     /// Invalid descriptor type: {0}
     InvalidDescriptorType(u8),
