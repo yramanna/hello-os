bb3d6041aba9a3aab82ac7e7638813163dadd806
Thalia Archibald <thalia@archibald.dev>
2025-10-06 18:12:35 -0600

    Include new modules

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 #![cfg_attr(not(test), no_std, no_main)]
 #![allow(static_mut_refs)] 
 
+mod cpu;
+mod error;
+mod gdt;
+mod interrupt;
+
 #[unsafe(no_mangle)]
 pub extern "C" fn rust_main() -> ! {
     println!("Hello from Rust!");
