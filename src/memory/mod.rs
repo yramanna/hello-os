8cec136efc0ad397ae05fd94bb8a3172783582f5
yramanna <yadu.nandan1195@gmail.com>
2025-11-09 20:05:56 -0700

    Update mod.rs

--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -78,56 +78,20 @@ unsafe impl GlobalAlloc for SimpleAllocator {
         // For allocations up to 4KB, allocate a 4KB page
         if layout.size() <= 4096 {
             match PAGE_ALLOCATOR.allocate_page(PageSize::Size4KB) {
-                Some(addr) => {
-                    addr as *mut u8
-                }
-                None => {
-                    null_mut()
-                }
+                Some(addr) => addr as *mut u8,
+                None => null_mut(),
             }
         } 
-        // For allocations larger than 4KB but less than 2MB, allocate multiple 4KB pages
+        // For allocations larger than 4KB but up to 2MB
         else if layout.size() <= 2 * 1024 * 1024 {
-            // Calculate how many 4KB pages we need
-            let pages_needed = (layout.size() + 4095) / 4096;
-            
-            // Try to allocate first page
-            if let Some(first_page) = PAGE_ALLOCATOR.allocate_page(PageSize::Size4KB) {
-                let mut contiguous = true;
-                let mut last_page = first_page;
-                
-                // Try to allocate remaining pages contiguously
-                for _ in 1..pages_needed {
-                    if let Some(page) = PAGE_ALLOCATOR.allocate_page(PageSize::Size4KB) {
-                        if page != last_page + 4096 {
-                            // Not contiguous, free what we got and fail
-                            contiguous = false;
-                            PAGE_ALLOCATOR.free_page(page, PageSize::Size4KB);
-                            break;
-                        }
-                        last_page = page;
-                    } else {
-                        contiguous = false;
-                        break;
-                    }
-                }
-                
-                if contiguous {
-                    first_page as *mut u8
-                } else {
-                    // Free all allocated pages
-                    let mut addr = first_page;
-                    while addr <= last_page {
-                        PAGE_ALLOCATOR.free_page(addr, PageSize::Size4KB);
-                        addr += 4096;
-                    }
-                    null_mut()
-                }
-            } else {
-                null_mut()
+            // For simplicity, just allocate a 2MB page if we need multiple 4KB pages
+            // This wastes memory but avoids complexity of tracking contiguous allocation
+            match PAGE_ALLOCATOR.allocate_page(PageSize::Size2MB) {
+                Some(addr) => addr as *mut u8,
+                None => null_mut(),
             }
         }
-        // For 2MB allocations
+        // For 2MB+ allocations
         else {
             match PAGE_ALLOCATOR.allocate_page(PageSize::Size2MB) {
                 Some(addr) => addr as *mut u8,
@@ -143,16 +107,11 @@ unsafe impl GlobalAlloc for SimpleAllocator {
         
         let addr = ptr as usize;
         
-        // Determine what size to free based on layout size
+        // Match the allocation strategy
         if layout.size() <= 4096 {
             PAGE_ALLOCATOR.free_page(addr, PageSize::Size4KB);
-        } else if layout.size() <= 2 * 1024 * 1024 {
-            // Free multiple 4KB pages
-            let pages_needed = (layout.size() + 4095) / 4096;
-            for i in 0..pages_needed {
-                PAGE_ALLOCATOR.free_page(addr + i * 4096, PageSize::Size4KB);
-            }
         } else {
+            // We allocated a 2MB page for anything > 4KB
             PAGE_ALLOCATOR.free_page(addr, PageSize::Size2MB);
         }
     }
